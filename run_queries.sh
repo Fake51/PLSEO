10534340d2027a0d512c5a647a0d1d0947d4adbe
Fake51 <peter@plind.dk>
2010-04-29 11:41:12 +0200

    updating to v0.8.1, cleaning up file structure, adding proper option handling

--- run_queries.sh
+++ run_queries.sh
@@ -36,32 +36,146 @@
      * @link      http://www.github.com/Fake51/PLSEO
      */
 
-
-if ($_SERVER['argc'] < 3)
-{
-    echo <<<TXT
+    $versiontext = <<<TXT
 PLSEO - Search Engine Position Checker
-Version: 0.8
+Version: 0.8.1
 Author: Peter Lind <peter@plphp.dk>
 
+TXT;
+
+    $helptext = <<<TXT
 Usage:
-    <run_script.sh> domain keyword(s) [search engine] [max pages to check] [user agent]
+    <run_script.sh> -d domain -k keyword [-spuoed]
+    <run_script.sh> -f domain_file -l keyword_file [-spuoed]
+Options:
+    -d domain
+        domain is the site that will be checked for in the SERPs. PLSEO will output
+        a one-dimensional array of results, unless -l is used. Note that -d takes
+        precedence over -f
+    -f domain_file
+        PLSEO will attempt to read domain_file and will check SERPs for all domains
+        found in the file. Put every domain on it's own line in the file. Output
+        will be a multidimensional array, with first dimension being keyword (if -l
+        is used) or domain (if -l is not used). Note that -d takes precedence over -f
+    -k keyword
+        The search engines will be checked for just the one keyword, and the script
+        will return when all search engines have been checked. Note that -k takes
+        precedence over -l
+    -l keyword_file
+        PLSEO will attempt to read keyword_file and will check SERPs for all
+        keywords found in the file. Put every keyword on it's own line in the file;
+        keywords consisting of multiple words need not be put in quotes. Output
+        will be a multidimensional array, with first dimension being keyword. Note
+        that -k takes precedence over -l
+    -s search_engine
+        If used, PLSEO will only query that particular searchengine. To see which
+        options are available for this option, us -e
+    -e
+        Lists all available engines
+    -p page_count
+        Number of SERPS to retrieve and search, defaults to 10. Note that beyond 18
+        PLSEO will pause for a minute, to avoid getting branded as a bot by engines
+    -u user_agent
+        User agent string to use. Currently defaults to Chrome
+    -o output_file
+        Write results to file instead of just outputting to STDOUT
+    -v
+        Switches the debug flag on, providing more verbose output
+    -h
+        Outputs this help text
 
 Example:
-    ./run_script.sh plphp.dk "typo3" 15
+    ./run_script.sh -d plphp.dk -k "freelance php"
 
 TXT;
-    exit;
-}
 
 require_once dirname(__FILE__) . '/searchclient.php';
 
-$site    = $_SERVER['argv'][1];
-$keyword = $_SERVER['argv'][2];
-$engine  = !empty($_SERVER['argv'][3]) ? $_SERVER['argv'][3] : null;
-$pages   = !empty($_SERVER['argv'][4]) && intval($_SERVER['argv'][4]) ? $_SERVER['argv'][4] : 10;
-$agent   = !empty($_SERVER['argv'][5]) ? $_SERVER['argv'][5] : null;
+$opts = getopt('d:k:f:l:s:p:u:o:hve');
 
-$client = new SearchClient($site, $keyword, $pages, $agent);
-var_dump($client->findRankings($engine));
+if (isset($opts['h']))
+{
+    echo $versiontext;
+    echo $helptext;
+    exit;
+}
 
+$client = new SearchClient();
+
+if (isset($opts['e']))
+{
+    echo $versiontext;
+    echo "Available engines:" . PHP_EOL;
+    foreach ($client->getEngines() as $engine)
+    {
+        echo "  " . $engine . PHP_EOL;
+    }
+    exit;
+}
+
+try
+{
+    if (empty($opts['d']) && empty($opts['f']))
+    {
+        echo $versiontext;
+        echo "Error: Missing one of -d or -f. See helptext (use -h)" . PHP_EOL;
+        exit;
+    }
+    elseif (!empty($opts['d']))
+    {
+        $client->setSite($opts['d']);
+    }
+    else
+    {
+        $client->setSiteFile($opts['f']);
+    }
+
+    if (empty($opts['k']) && empty($opts['l']))
+    {
+        echo $versiontext;
+        echo "Error: Missing one of -k or -l. See helptext (use -h)" . PHP_EOL;
+        exit;
+    }
+    elseif (!empty($opts['k']))
+    {
+        $client->setKeyword($opts['k']);
+    }
+    else
+    {
+        $client->setKeywordFile($opts['l']);
+    }
+
+    if (!empty($opts['s']))
+    {
+        $client->setSearchEngine($opts['s']);
+    }
+
+    if (!empty($opts['p']))
+    {
+        $client->setPages($opts['p']);
+    }
+
+    if (!empty($opts['o']))
+    {
+        $client->setOutputFile($opts['o']);
+    }
+
+    if (!empty($opts['u']))
+    {
+        $client->setUserAgent($opts['u']);
+    }
+
+    if (isset($opts['v']))
+    {
+        $client->setDebugFlag();
+    }
+
+    var_dump($client->findRankings());
+}
+catch (Exception $e)
+{
+    echo $versiontext;
+    echo "PLSEO failed with an exception. Message:" . PHP_EOL;
+    echo "  " . $e->getMessage();
+    exit;
+}
--- searchclient.php
+++ searchclient.php
@@ -35,6 +35,8 @@
      * @link      http://www.github.com/Fake51/PLSEO
      */
 
+require_once dirname(__FILE__) . '/searchengine.php';
+
     /**
      * client class for querying search engines
      *
@@ -45,8 +47,10 @@ class SearchClient
 {
     private $_site;
     private $_keyword;
-    private $_max_pages;
+    private $_max_pages = 10;
     private $_user_agent = '';
+    private $_search_engine;
+    private $_debugging = false;
 
     const GOOGLECOM = 'GoogleComEngine';
     const GOOGLEDK = 'GoogleDkEngine';
@@ -70,15 +74,126 @@ class SearchClient
         self::BINGUS,
     );
 
-    public function __construct($site, $keyword, $pages = 10, $useragent = null)
+    /**
+     * sets the search engine to use. Reduces the number of engines to use from
+     * all to just this one
+     *
+     * @param string $engine
+     *
+     * @throws Exception
+     * @access public
+     * @return void
+     */
+    public function setSearchEngine($engine)
     {
-        $this->_site = $site;
-        $this->_keyword = $keyword;
-        $this->_max_pages = $pages;
-        if ($useragent)
+        if (!in_array($engine, $this->_engines))
         {
-            $this->_user_agent = $useragent;
+            throw new Exception("No such engine" . PHP_EOL);
         }
+        $this->_search_engine = $engine;
+    }
+
+    public function getEngines()
+    {
+        return $this->_engines;
+    }
+
+    /**
+     * sets the number of SERPs to retrieve - defaults to 10
+     *
+     * @param int $pages
+     *
+     * @throws Exception
+     * @access public
+     * @return void
+     */
+    public function setPages($pages)
+    {
+        if (intval($pages) == 0)
+        {
+            throw new Exception("Invalid variable for pages specified" . PHP_EOL);
+        }
+        $this->_max_pages = $pages;
+    }
+
+    /**
+     * sets the user agent to use when querying search engines
+     *
+     * @param string $useragent
+     *
+     * @throws Exception
+     * @access public
+     * @return void
+     */
+    public function setUserAgent($useragent)
+    {
+        if (!is_string($useragent) || strlen($useragent) == 0)
+        {
+            throw new Exception("Invalid user agent specified" . PHP_EOL);
+        }
+        $this->_user_agent = $useragent;
+    }
+
+    /**
+     * turns on debugging mode, meaning more output
+     *
+     * @access public
+     * @return void
+     */
+    public function setDebugFlag()
+    {
+        $this->_debugging = true;
+    }
+
+    /**
+     * sets a single site to query SERPs for
+     *
+     * @param string $site
+     *
+     * @throws Exception
+     * @access public
+     * @return void
+     */
+    public function setSite($site)
+    {
+        if (!is_string($site) || strlen($site) == 0)
+        {
+            throw new Exception("Invalid site specified" . PHP_EOL);
+        }
+        $this->_site = $site;
+    }
+
+    /**
+     * sets a single keyword to query SERPs for
+     *
+     * @param string $keyword
+     *
+     * @throws Exception
+     * @access public
+     * @return void
+     */
+    public function setKeyword($keyword)
+    {
+        if (!is_string($keyword) || strlen($keyword) == 0)
+        {
+            throw new Exception("Invalid keyword specified" . PHP_EOL);
+        }
+        $this->_keyword = $keyword;
+    }
+
+    public function setOutputFile($outputfile)
+    {
+        throw new Exception("Functionality not implemented yet" . PHP_EOL);
+    }
+
+    public function setKeywordFile($keywordfile)
+    {
+        throw new Exception("Functionality not implemented yet" . PHP_EOL);
+    }
+
+    public function setSiteFile($sitefile)
+    {
+        throw new Exception("Functionality not implemented yet" . PHP_EOL);
     }
 
     /**
@@ -89,25 +204,15 @@ class SearchClient
      * @access public
      * @return array
      */
-    public function findRankings($engine = null)
+    public function findRankings()
     {
-        if ($engine)
+        if (empty($this->_keyword) || empty($this->_site))
         {
-            if (!in_array($engine, $this->_engines))
-            {
-                throw new Exception("No such engine");
-            }
+            throw new Exception("Lacking keyword or site" . PHP_EOL);
         }
-        if ($engine)
+        if (!empty($this->_search_engine))
         {
-            try
-            {
-                $this->_registerEngine($engine);
-            }
-            catch (Exception $e)
-            {
-                die($e->getMessage() . PHP_EOL);
-            }
+            $this->_registerEngine($engine);
         }
         else
         {
@@ -144,17 +249,19 @@ class SearchClient
      */
     private function _registerEngine($engine)
     {
-        require_once dirname(__FILE__) . '/searchengine.php';
         try
         {
-            require_once dirname(__FILE__) . '/' . strtolower($engine) . '.php';
+            require_once dirname(__FILE__) . '/engines/' . strtolower($engine) . '.php';
             $engine_object = new $engine($this->_site, $this->_keyword, $this->_max_pages);
             $this->_engines_running[$engine] = $engine_object;
             if ($this->_user_agent)
             {
                 $engine_object->setUserAgent($this->_user_agent);
             }
-            //$engine_object->setDebugMode(true);
+            if (!empty($this->_debugging))
+            {
+                $engine_object->setDebugMode(true);
+            }
         }
         catch (Exception $e)
         {
